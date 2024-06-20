e856178edb197c1ed39e3e31fde9f48e9770fd0e
Mohamad Haji Khalil <mohamad.hk678@gmail.com>
2024-06-20 17:43:54 +0200

    Longest Common Prefix

---
+++ easy/Longest Common Prefix.py
@@ -0,0 +1,18 @@
+#not my solution src = https://leetcode.com/problems/longest-common-prefix/solutions/3273176/python3-c-java-19-ms-beats-99-91/
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        ans=""
+        v = sorted(strs)
+        first = v[0]
+        last = v[-1]
+        for i in range(min(len(first), len(last))):
+            if(first[i]!=last[i]): 
+                return ans
+            ans += first[i]
+        return ans
+
+def main():
+    s = ["dog","racecar","car"]
+    solver = Solution()
+    print(solver.longestCommonPrefix(s))
+main()
